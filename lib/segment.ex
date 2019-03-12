41547d37f5ac2651a4b23d3d61b3c0360d4c139d
Zamith <zamith.28@gmail.com>
2019-03-12 17:47:16 +0000

    Properly implement the supervisor module

--- lib/segment.ex
+++ lib/segment.ex
@@ -2,12 +2,15 @@ defmodule Segment do
   use Supervisor
 
   def start_link(write_key) do
+    Supervisor.start_link(__MODULE__, write_key, name: __MODULE__)
+  end
+
+  def init(write_key) do
     children = [
       worker(Segment.Keychain, [write_key]),
       Segment.Analytics.Server
     ]
 
-    opts = [strategy: :one_for_one, name: __MODULE__]
-    Supervisor.start_link(children, opts)
+    Supervisor.init(children, strategy: :one_for_one)
   end
 end
